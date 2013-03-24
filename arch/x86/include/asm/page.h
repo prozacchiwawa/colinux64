53f3bff0c25b5202fcb504f51989151f8572219e
arty <art.yerkes@gmail.com>
2013-03-23 22:05:24 -0700

    WIP.  Going to convert to CONFIG_COOPERATIVE as the original.

--- arch/x86/include/asm/page.h
+++ arch/x86/include/asm/page.h
@@ -40,6 +40,10 @@ static inline void copy_user_page(void *to, void *from, unsigned long vaddr,
 #ifndef CONFIG_COLINUX_KERNEL
 #define __pa(x)		__phys_addr((unsigned long)(x))
 #define __pa_nodebug(x)	__phys_addr_nodebug((unsigned long)(x))
+#else
+#define __pa(x)         colinux_real_v2p(((unsigned long)(x)))
+#define __pa_nodebug(x) __pa(x)
+#endif
 /* __pa_symbol should be used for C visible symbols.
    This seems to be the official gcc blessed way to do such arithmetic. */
 /*
@@ -51,11 +55,6 @@ static inline void copy_user_page(void *to, void *from, unsigned long vaddr,
  */
 #define __pa_symbol(x) \
 	__phys_addr_symbol(__phys_reloc_hide((unsigned long)(x)))
-#else
-#define __pa(x)         colinux_real_v2p(((unsigned long)(x)))
-#define __pa_nodebug(x) __pa(x)
-#define __pa_symbol(x)  __pa(x)
-#endif
 
 #ifdef CONFIG_COLINUX_KERNEL
 extern unsigned long colinux_real_p2v(unsigned long pa);
